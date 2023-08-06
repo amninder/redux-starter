4d3ae6d548ac9cb842ebc2365036b813eb8bc4dc
Amninder S Narota <anarota@truveris.com>
2023-08-06 00:03:27 -0400

    redux store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("this is a sample sentence");
+import { bugAdded, bugResolved } from "./store/bugs";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
+
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed!", store.getState());
+});
+
+store.dispatch(bugAdded("bug1"));
+store.dispatch(bugAdded("bug2"));
+store.dispatch(bugAdded("bug3"));
+store.dispatch(bugResolved(2));
+
+unsubscribe();
--- webpack.config.ts
+++ webpack.config.ts
@@ -4,27 +4,28 @@ import "webpack-dev-server";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const config: webpack.Configuration = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.bundle.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+    mode: "development",
+    devtool: "source-map",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.bundle.js",
     },
-    compress: true,
-    port: 3000,
-    client: {
-      logging: "info",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public/index.html"),
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 3000,
+        client: {
+            logging: "none",
+        },
     },
-  },
 };
 
 export default config;
